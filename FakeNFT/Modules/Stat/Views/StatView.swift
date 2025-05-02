c297bdcd41d3cc4f33503d8756b5daa0dee91340
Valery Zvonarev <furysound1@yandex.ru>
2025-05-02 21:45:10 +0300

    [feature] Added sorting button with confirmationDialog code, sorting popup (TODO: add sorting code), DarkTheme (sorting icon for light and dark)

--- FakeNFT/Modules/Stat/Views/StatView.swift
+++ FakeNFT/Modules/Stat/Views/StatView.swift
@@ -9,30 +9,59 @@ import SwiftUI
 
 struct StatView: View {
     @ObservedObject var viewModel = ProfileStatViewModel()
+    @State private var showActionSheet = false
+    @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
-        List {
-            ForEach(0..<viewModel.profileStatViews.count) { value in
-//            ForEach(viewModel.profileStatViews, id: \.self) { value in
-                HStack {
-                    Text("\(value + 1)")
-//                    Spacer()
-                    StatCellView(profile: viewModel.profileStatViews[value])
+        VStack(alignment: .trailing) {
+            HStack {
+                Button(action: {
+                    showActionSheet.toggle()
+                }) {
+                    Image(colorScheme == .dark ? "sortingButtonDark" : "sortingButtonLight") // Иконка меню
                 }
+                .confirmationDialog("Сортировка", isPresented: $showActionSheet, titleVisibility: .visible) {
+                    Button("По имени") { print("Option 1 - Сортировка по имени") }
+                    Button("По рейтингу") { print("Option 2 - Сортировка по рейтингу") }
+                    Button("Закрыть", role: .cancel) {}
+                }
+                //                .actionSheet(isPresented: $showActionSheet) {
+                //                    ActionSheet(title: Text(""), message: Text("Сортировка"), buttons: [
+                //                        .default(Text("По имени")) { print("Option 1 - Сортировка по имени") },
+                //                        .default(Text("По рейтингу")) { print("Option 2 - Сортировка по рейтингу") },
+                //                        .cancel(Text("Закрыть"))
+                //                    ])
+                //                }
             }
-            .listRowBackground(Color.clear)
-            .listRowSeparator(.hidden)
-            .padding(.bottom, 8)
-            .listRowInsets(EdgeInsets())
+            .padding(.top, 14.7)
+            .padding(.bottom, 14.7)
+            List {
+                ForEach(0..<viewModel.profileStatViews.count) { value in
+                    //            ForEach(viewModel.profileStatViews, id: \.self) { value in
+                    HStack {
+                        Text("\(value + 1)")
+                        StatCellView(profile: viewModel.profileStatViews[value])
+                    }
+                }
+                .listRowBackground(Color.clear)
+                .listRowSeparator(.hidden)
+                .padding(.bottom, 8)
+                .listRowInsets(EdgeInsets())
+            }
+            .listStyle(.plain)
+            .ignoresSafeArea(edges: .bottom)
+            .scrollIndicators(.never)
         }
-        .listStyle(.plain)
-        .ignoresSafeArea(edges: .bottom)
         .padding(.horizontal, 16)
-        .padding(.top, 1)
-        .scrollIndicators(.never)
     }
 }
 
-#Preview {
+#Preview("Light Theme") {
     StatView()
+        .preferredColorScheme(.light)
+}
+
+#Preview("Dark Theme") {
+    StatView()
+        .preferredColorScheme(.dark)
 }
